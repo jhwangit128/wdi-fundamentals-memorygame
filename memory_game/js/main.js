7bdcafdd85ca9ce7ec7bc8918b966768029f5295
Jiha Hwang <jiha_hwang@jihas-mbp.lan>
2019-06-17 01:30:29 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,34 @@
-console.log("Up and running!");
+// console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+// var cardOne = "queen";
+// var cardTwo = "queen";
+// var cardThree = "king";
+// var cardFour = "king";
 
-console.log ("User flipped " + cardOne);
-console.log ("User flipped " + cardFour);
+// console.log ("User flipped " + cardOne);
+// console.log ("User flipped " + cardFour);
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+// function flipCard (){
+// 	var cardId = this.setAttribute('data-id');
+// }
+
+var cardOne = cards[0];
+cardsInPlay.push("queen");
+console.log ("User flipped queen");
+
+var cardTwo = cards[3];
+cardsInPlay.push("king");
+console.log ("User flipped king");
+
+if (cardsInPlay.length ===2) {
+	if(cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.")
+	}
+}
+
